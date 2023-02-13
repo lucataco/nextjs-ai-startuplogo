130c401470c2e74f4f9ee434bfb547bac3de964e
Luis C <me@lucataco.com>
2023-02-13 01:56:13 -0500

    Plausible Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Script from 'next/script'
 import '../styles/styles.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return  (
+    <>
+      <Script  defer data-domain="startuplogos.up.railway.app" src="https://data.lucata.co/js/script.js" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
